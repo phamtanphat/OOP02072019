ffd4bbb4438094e7ffffd62e8a6258aa8f1de160
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-27 18:03:29 +0700

    overide

--- app/src/main/java/khoapham/ptp/phamtanphat/oop02072019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/oop02072019/MainActivity.java
@@ -15,8 +15,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Nhanvien nhanvien = new Nhanvien("Nguyen Van A",10 ,"Nhan vien cap A");
+        nhanvien.cuchi("Lam viec theo team A");
 
-
+        //Overide
+        //1 : Khong thay đổi ten phương thức , tham số truyền vào , giá trị trả về
+        //2 : Khi 2 phương thức khác vị trí
 
     }
 }
--- app/src/main/java/khoapham/ptp/phamtanphat/oop02072019/Nhanvien.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/oop02072019/Nhanvien.java
@@ -1,12 +1,21 @@
 package khoapham.ptp.phamtanphat.oop02072019;
 
 
+import android.util.Log;
+
 public class Nhanvien extends Person{
+
     String chucvi;
 
     public Nhanvien(String name, int age , String chucvi) {
         super(name, age);
         this.chucvi = chucvi;
+    }
+
+
+    @Override
+    public void cuchi(String hanhdong) {
+        Log.d("BBB","Nhan vien " + hanhdong);
     }
 
 }
