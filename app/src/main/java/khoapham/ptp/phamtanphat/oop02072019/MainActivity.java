90b9d6f70f149e69975ac51a8969069af2a75972
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-27 18:12:19 +0700

    overload

--- app/src/main/java/khoapham/ptp/phamtanphat/oop02072019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/oop02072019/MainActivity.java
@@ -14,12 +14,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Nhanvien nhanvien = new Nhanvien("Nguyen Van A",10 ,"Nhan vien cap A");
-        nhanvien.cuchi("Lam viec theo team A");
+//        Nhanvien nhanvien = new Nhanvien("Nguyen Van A",10 ,"Nhan vien cap A");
+//        nhanvien.cuchi("Lam viec theo team A");
 
         //Overide
         //1 : Khong thay đổi ten phương thức , tham số truyền vào , giá trị trả về
         //2 : Khi 2 phương thức khác vị trí
 
+        //Overload
+        //1 : Phai thêm xóa hoặc chỉnh sửa kiếu dữ liệu truyền vào
+        //2 : 2 phương thức phải cùng nơi tạo ra
+        //3 : Có quyền thay đổi giá trị trả về của function , thân hàm
+       showLog(1);
+    }
+    public void showLog(String key){
+        Log.d("BBB",key);
+    }
+    public void showLog(int value){
+        Log.d("BBB", String.valueOf(value));
     }
 }
